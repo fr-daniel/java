5312914dbd231c3b10a11a1cff9345f26afaff21
Daniel Lima <fr.daniel.lima@gmail.com>
2017-12-26 00:03:47 -0300

    Capítulo 3.1

---
+++ java_se_8/capitulo3/3.1/Questao1.java
@@ -0,0 +1,12 @@
+/**
+ * Questao1
+ * a) Erro de compilação.
+ */
+public class Questao1 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }   
+        System.out.println(i);
+    }
+}
---
+++ java_se_8/capitulo3/3.1/Questao3.java
@@ -0,0 +1,13 @@
+/**
+ * Questao3
+ * c) Compila e roda, imprimindo de 0 até 19 e depois 15.
+ */
+public class Questao3 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+        int i = 15;
+        System.out.println(i);
+    }
+}
